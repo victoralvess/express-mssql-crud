ac30958924966d1d9b2198f77d6fde35f3faa584
Victor Alves <vsilva.programador@gmail.com>
2018-04-23 09:46:44 -0300

    Add /contacts endpoint

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 require('./config/knex');
 const Contact = require('./models/contact');
 
+app.get('/contacts', (req, res) => {
+  Contact.query()
+    .then(contacts => res.send(contacts))
+    .catch(error => res.status(500).send(error));
+});
+
 /*
  * // TESTING CONNECTION
   Contact.query().then(contacts => {
