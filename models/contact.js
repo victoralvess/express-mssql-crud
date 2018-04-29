ccfe07a07a8144f94bd28b7650ec5537b4f094d2
Victor Alves <vsilva.programador@gmail.com>
2018-04-29 17:21:51 -0300

    Add schema validation

--- models/contact.js
+++ models/contact.js
@@ -8,6 +8,18 @@ class Contact extends Model {
   get fullName() {
     return `${this.first_name} ${this.last_name}`;
   }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['first_name', 'last_name', 'phone'],
+      properties: {
+        id: { type: 'integer' },
+        first_name: { type: 'string', minLength: 3, maxLength: 80 },
+        last_name: { type: 'string', minLength: 2, maxLength: 80 }
+      }
+    };
+  }
 }
 
 module.exports = Contact;
