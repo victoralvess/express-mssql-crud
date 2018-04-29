82905e1293dac83d17528afe4ebd358c0d4b3fcc
Victor Alves <vsilva.programador@gmail.com>
2018-04-29 17:21:19 -0300

    Add fullName function to the contact model

--- models/contact.js
+++ models/contact.js
@@ -4,6 +4,10 @@ class Contact extends Model {
   static get tableName() {
     return 'contacts';
   }
+
+  get fullName() {
+    return `${this.first_name} ${this.last_name}`;
+  }
 }
 
 module.exports = Contact;
