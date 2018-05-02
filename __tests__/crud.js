cd735ee267b877a36a6af9c98b10781cff35e339
Victor Alves <vsilva.programador@gmail.com>
2018-05-02 19:39:42 -0300

    Add /contacts/1 test

--- __tests__/crud.js
+++ __tests__/crud.js
@@ -25,15 +25,35 @@ describe('testing GET request at /contacts', () => {
 });
 
 describe('testing GET request at /contacts/{id}', () => {
-  test('return 404 status code if the contact does not exist', (done) => {
-    http.request({
-      ...httpOptions,
-      path: '/contacts/0'
-    }, res => {
-      expect(res.statusCode).toBe(404);
-      done();
-    }).end();
-  })
+  test('return 404 status code if the contact does not exist', done => {
+    http
+      .request(
+        {
+          ...httpOptions,
+          path: '/contacts/0',
+        },
+        res => {
+          expect(res.statusCode).toBe(404);
+          done();
+        },
+      )
+      .end();
+  });
+
+  test('return 200 status code if the contact exists', done => {
+    http
+      .request(
+        {
+          ...httpOptions,
+          path: '/contacts/1',
+        },
+        res => {
+          expect(res.statusCode).toBe(200);
+          done();
+        },
+      )
+      .end();
+  });
 });
 
 describe('testing POST request at /contacts/add', () => {
@@ -45,28 +65,26 @@ describe('testing POST request at /contacts/add', () => {
       'Content-type': 'application/json',
     },
   };
-   
+
   test('returned status code have to be 201 when a contact is added', done => {
-    const req = http.request(
-      options,
-      res => {
-        expect(res.statusCode).toBe(201);
-        done();
-      },
+    const req = http.request(options, res => {
+      expect(res.statusCode).toBe(201);
+      done();
+    });
+    req.write(
+      '{ "first_name": "First Name", "last_name": "Last Name", "phone": "555-555-555" }',
     );
-    req.write('{ "first_name": "First Name", "last_name": "Last Name", "phone": "555-555-555" }');
     req.end();
   });
 
   test('verifies if Location header was set after a contact addition', done => {
-    const req = http.request(
-      options,
-      res => {
-        expect(res.headers.location).not.toBeFalsy();
-        done();
-      }
+    const req = http.request(options, res => {
+      expect(res.headers.location).not.toBeFalsy();
+      done();
+    });
+    req.write(
+      '{ "first_name": "First Name", "last_name": "Last Name", "phone": "555-555-555" }',
     );
-    req.write('{ "first_name": "First Name", "last_name": "Last Name", "phone": "555-555-555" }');
     req.end();
   });
 });
